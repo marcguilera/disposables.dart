6e7e673db77cb49d928f4a38c2652b87a0a0c4da
deakjahn <deakjahn@gmail.com>
2021-03-12 00:41:38 +0100

    Null safety.

--- lib/src/collection/_disposable_list.dart
+++ lib/src/collection/_disposable_list.dart
@@ -1,7 +1,6 @@
 part of 'package:disposables/disposables.dart';
 
-class _DisposableList<D extends Disposable> extends DelegatingList<D>
-    implements DisposableCollection<D> {
+class _DisposableList<D extends Disposable> extends DelegatingList<D> implements DisposableCollection<D> {
   @override
   final List<D> delegate;
   final bool autoClear;
--- lib/src/collection/disposable_collection.dart
+++ lib/src/collection/disposable_collection.dart
@@ -1,16 +1,15 @@
 part of 'package:disposables/disposables.dart';
 
 /// Represents a collection
-abstract class DisposableCollection<D extends Disposable>
-    implements Disposable, List<D> {
+abstract class DisposableCollection<D extends Disposable> implements Disposable, List<D> {
   /// Creates a new [DisposableCollection].
-  factory DisposableCollection({bool autoClear}) {
+  factory DisposableCollection({bool? autoClear}) {
     return DisposableCollection.from([], autoClear: autoClear);
   }
 
   /// Creates a [DisposableCollection] from list for [Disposable] instances.
-  factory DisposableCollection.from(Iterable<D> other, {bool autoClear}) {
-    checkNotNull(other, message: () => "other can't be null");
+  factory DisposableCollection.from(Iterable<D> other, {bool? autoClear}) {
+    ArgumentError.checkNotNull(other, "other can't be null");
     return _DisposableList(other.toList(), autoClear ?? false);
   }
 }
